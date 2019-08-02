9326fb97c8c626325a2cd810ea3a78fe4da7fa2d
Dagg <daggintosh@outlook.com>
2019-08-02 11:12:41 -0700

    bootstrap

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import foxface from './foxface.png'
+
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="default" />
+      <SafeAreaView style={{flex: 1,backgroundColor: '#111111'}}>
+      
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
