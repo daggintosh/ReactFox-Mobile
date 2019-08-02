5376eb43a499276d37e550abef1b3498a372f0f7
Dagg <daggintosh@outlook.com>
2019-08-02 12:39:06 -0700

    Functional button

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import React, {Fragment} from 'react';
-import { SafeAreaView, StatusBar, Image, View } from 'react-native';
+import React, {Fragment, useState} from 'react';
+import { SafeAreaView, StatusBar, Image, View, TouchableOpacity } from 'react-native';
 import foxface from './foxface.png'
 
 const App = () => {
+  const [imgkey, setkey] = useState(1);
+  const refresh = () => setkey(imgkey + 1);
   return (
     <Fragment>
       <StatusBar barStyle="default" />
@@ -10,7 +12,9 @@ const App = () => {
         <Image source={{uri: 'https://dagg.xyz/randomfox/images/' + (Math.floor(Math.random() * 123) + 1) + '.jpg'}} style={{flex: 1, height: undefined, width: undefined}} resizeMode="contain"/>
       </SafeAreaView>
       <View style={{flex: 0.1, justifyContent: 'center', alignItems: "center", marginBottom: 0, backgroundColor: "#222222"}}>
-        <Image source={foxface} style={{flex: 1, maxHeight: 50}} resizeMode="contain"  />
+        <TouchableOpacity onPress={refresh} style={{maxWidth: 50, alignItems: "center"}}>
+          <Image source={foxface} style={{flex: 1, maxHeight: 50}} resizeMode="contain" key={imgkey}/>
+        </TouchableOpacity>
       </View>
     </Fragment>
   );
