c041e1a4cb55775e0673ef1c6e5b7f97d183e624
Dagg <daggintosh@outlook.com>
2019-08-02 12:26:43 -0700

    Random Fox System

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import { SafeAreaView, StyleSheet, StatusBar, Image } from 'react-native';
+import { SafeAreaView, StatusBar, Image, View } from 'react-native';
 import foxface from './foxface.png'
 
 const App = () => {
@@ -7,14 +7,14 @@ const App = () => {
     <Fragment>
       <StatusBar barStyle="default" />
       <SafeAreaView style={{flex: 1,backgroundColor: '#111111'}}>
-        <Image source={foxface}  />
+        <Image source={{uri: 'https://dagg.xyz/randomfox/images/' + (Math.floor(Math.random() * 123) + 1) + '.jpg'}} style={{flex: 1, height: undefined, width: undefined}} resizeMode="contain"/>
       </SafeAreaView>
+      <View style={{flex: 0.1, justifyContent: 'center', alignItems: "center", marginBottom: 0, backgroundColor: "#222222"}}>
+        <Image source={foxface} style={{flex: 1, maxHeight: 50}} resizeMode="contain"  />
+      </View>
     </Fragment>
   );
 };
 
-const styles = StyleSheet.create({
-  
-});
 
 export default App;
