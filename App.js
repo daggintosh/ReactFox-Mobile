0669e9b6db37c5b629ac1affca14d29f1e78ef7b
Dagg <daggintosh@outlook.com>
2019-08-02 17:23:07 -0700

    status

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Fragment, useState} from 'react';
-import { SafeAreaView, StatusBar, Image, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Image, View, TouchableOpacity } from 'react-native';
 import foxface from './foxface.png'
 
 const App = () => {
@@ -7,7 +7,6 @@ const App = () => {
   const refresh = () => setkey(imgkey + 1);
   return (
     <Fragment>
-      <StatusBar barStyle="light-content" />
       <SafeAreaView style={{flex: 1,backgroundColor: '#111111'}}>
         <Image source={{ uri: "https://dagg.xyz/randomfox/images/" + (Math.floor(Math.random() * 123) + 1) + ".jpg" }} style={{flex: 1, height: undefined, width: undefined}} resizeMode="contain"/>
       </SafeAreaView>
